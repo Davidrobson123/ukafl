cc4c052bc2441745d459d0e1149f715d589f00d1
David <davidrobson2106@hotmail.co.uk>
2019-07-23 23:00:04 +0100

    changes

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import '../../App.scss';
+
+export default class About extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        )
+    };
+}
---
+++ src/components/pages/Standings.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import '../../App.scss';
+import PageHeader from "../main/PageHeader";
+import SocialLinks from "../main/SocialLinks";
+import Sponsors from "../main/Sponsors";
+import Header from '../../components/common/Header';
+import Footer from '../../components/common/Footer';
+
+export default class Standings extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+      const script = document.createElement("script");
+
+      script.src = "https://api.leaguerepublic.com/l/client/api/cs1.js";
+      script.async = true;
+
+      document.body.appendChild(script);
+
+      window.lrcode = '189826185'
+    }
+
+    render() {
+
+        return (
+            <div className="main">
+                <Header />
+
+                <PageHeader title="Standings / Fixtures"/>
+
+                <div className="league-standings">
+                    <div  id="lrep189826185" style={{width: '50%'}} >Data Loading...</div>
+                </div>
+
+                <Sponsors/>
+
+                <SocialLinks />
+
+
+                <Footer />
+
+            </div>
+        )
+    };
+}
