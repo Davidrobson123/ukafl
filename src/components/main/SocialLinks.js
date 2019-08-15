8cdabbbaf41137a2e68c108c53c630bf6346bf4d
David Robson <davidrobson2106@hotmail.co.uk>
2019-08-15 16:58:19 +0100

    changes to front page

--- src/components/main/SocialLinks.js
+++ src/components/main/SocialLinks.js
@@ -12,13 +12,13 @@ const SocialLinks = props => {
         <div className="line">
           <div className="m-10 l-6 xl-4 center">
             <div className="margin">
-              <a className="s-12 m-4 margin-s-bottom" href="/">
+              <a className="s-12 m-4 margin-s-bottom" href="https://www.facebook.com/ukafl/">
                 <img className="facebook right" src={Facebook} alt=""/>
               </a>
-              <a className="s-12 m-4" href="/">
+              <a className="s-12 m-4" href="https://twitter.com/UK_AFL">
                 <img className="twitter" src={Twitter} alt="" />
               </a>
-              <a className="s-12 m-4" href="/">
+              <a className="s-12 m-4" href="https://www.youtube.com/channel/UC2uPm-jewf-IDC6kgmwoOLw">
                 <img className="youtube" src={Youtube} alt="" />
               </a>
             </div>
